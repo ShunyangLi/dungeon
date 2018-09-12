fcabfe9eb989a64b7a3191651e274762e2f01bda
StackOfDingFeng <z5127693@student.unsw.edu.au>
2018-09-12 14:31:27 +1000

    change order

--- ass2/src/ass2/Objects.java
+++ ass2/src/ass2/Objects.java
@@ -6,26 +6,24 @@ public class Objects {
     public static final int road = 0;
     public static final int wall = 1;
     public static final int exit = 2;
-    public static final int treasure = 3;
-    public static final int door = 4;
-    public static final int key = 5;
-    public static final int boulder = 6;
-    public static final int floor_switch = 7;
-    // 待定，bloom是否可以自带属性
-    public static final int bloom = 8;
+    public static final int boulder = 3;
+    public static final int floor_switch = 4;
+    public static final int door = 5;
+    public static final int treasure = 6;
+    public static final int key = 7;
+
+    // this is props player can pick up
+    public static final int bomb = 8;    // 待定，bloom是否可以自带属性
     public static final int pit = 9;
+    public static final int Sword = 10;
+    public static final int Arrow = 11;
+    public static final int Invincibility = 12;
+    public static final int Hover = 13;
 
     // This is the players
-    public static final int player = 10;
-    public static final int hunter = 11;
-    public static final int strategist = 12;
-    public static final int hound = 13;
-    public static final int coward = 14;
-    public static final int Sword = 15;
-    public static final int Arrow = 16;
-    public static final int Invincibility = 17;
-    public static final int Hover = 18;
-
-
-
+    public static final int player = 14;
+    public static final int hunter = 15;
+    public static final int strategist = 16;
+    public static final int hound = 17;
+    public static final int coward = 18;
 }
