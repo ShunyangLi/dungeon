621bf421699bab07f2b6d1f260b3b8a6a8f8e6bf
ShunyangLi0812 <z5139935@student.unsw.edu.au>
2018-09-15 15:03:17 +1000

    关于enemy的部分的重构还没想好怎么写，先保存一份

--- ass2/src/ass2/GameSystem.java
+++ ass2/src/ass2/GameSystem.java
@@ -49,40 +49,6 @@ public class GameSystem {
             map.showMap(player.getPosition());
         }
 
-
-        // 新的大宝剑测试
-//        Sword sword = new Sword(map);
-////        sword.setPositionOnMap(1,0);
-////
-////        // 没啥屁用
-//        Bomb bomb = new Bomb(map);
-//        Arrow arrow = new Arrow(map);
-//        Treasure treasure = new Treasure(map);
-//        Key key = new Key(map);
-//
-//
-//        // 设置背包呢
-//        Bag bag = new Bag(sword,arrow,bomb,treasure, key);
-//
-//        // 设置player，等待测试
-//        Player player = new Player(map, bag);
-////        Sword sword1 = new Sword(map);
-////        sword1.setPositionOnMap(2,0);
-//        map.showMap(player.getPosition());
-
-
-//        player.moveDown();
-////        System.out.println();
-//        map.showMap(player.getPosition());
-//        System.out.println("Go right!");
-//        player.moveRight();
-//        map.showMap(player.getPosition());
-//        player.moveDown();
-//        map.showMap(player.getPosition());
-//        player.moveDown();
-//        map.showMap(player.getPosition());
-//        player.moveDown();
-//        map.showMap(player.getPosition());
     }
 
 }
